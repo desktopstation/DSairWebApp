6dea355b35ecc66e15e5c01f116668a8f9204007
Yasuhiro <21124360+Yasuhiro-Kawai@users.noreply.github.com>
2018-12-31 08:47:06 +0900

    Modify voltage and current display format

--- js/dsair_status_view.js
+++ js/dsair_status_view.js
@@ -1,3 +1,4 @@
+// 
 var DsairStatusView = function () {
     this._device = null;
 };
@@ -6,10 +7,16 @@ DsairStatusView.prototype.setDevice = function (inDevice) {
     this._device = inDevice;
 };
 
+DsairStatusView.prototype.formatNumber = function (n) {
+    let intPart = Math.trunc(n / 10);
+    let tenthPart = n - (intPart * 10);
+    return intPart.toString() + '.' + tenthPart.toString();
+};
+
 DsairStatusView.prototype.setStatus = function (arg) {
 
-    $('#status_volt').text('Track Voltage: ' + (arg.statusVolt / 10).toString() + '[V]');
-    $('#status_current').text('Out Current: ' + (arg.statusCurrent / 10).toString() + '[A]');
+    $('#status_volt').text('Track Voltage: ' + this.formatNumber(arg.statusVolt) + '[V]');
+    $('#status_current').text('Out Current: ' + this.formatNumber(arg.statusCurrent) + '[A]');
     $('#status_power').text('Track Power: ' + (arg.statusPower == 'Y' ? 'ON' : 'OFF'));
     $('#status_firmver').text('Firmware: ver.' + arg.statusFirmVer);
     $('#status_error').text('Error: ' + this._device.ErrorString(arg.statusError));
