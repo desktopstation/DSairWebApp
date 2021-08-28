5349c138c37c2f06a422b1577e6e8ae6b9e3a591
Yaasan <46058494+desktopstation@users.noreply.github.com>
2021-08-29 07:12:25 +0900

    r3.3差し替え
    r3.3バージョンへの差し替え

--- js/device.js
+++ js/device.js
@@ -1,61 +1,78 @@
-var Device = function () {
-	this._noVibration = 0;
-};
 
-Device.prototype._noVibration = 0;
+var gNoVibration = 0;
 
-Device.prototype.Vibrate = function () {
-	if (this._noVibration == 1) {
+
+function DEVICE_Vibrate()
+{
+	if( gNoVibration == 1)
+	{
 		return;
 	}
-
-	setTimeout(function () {
-		if (window.navigator.vibrate) {
+	
+	setTimeout(function(){
+		if(window.navigator.vibrate)
+		{
 			window.navigator.vibrate(25);
-		} else if (window.navigator.webkitVibrate) {
+		}
+		else if(window.navigator.webkitVibrate)
+		{
 			window.navigator.webkitVibrate(25);
 		}
 	}, 0);
 
-};
+}
 
-Device.prototype.ErrorString = function (inErrNo) {
-	var aStr = '';
 
-	switch (inErrNo) {
+
+function DEVICE_ErrorString(inErrNo)
+{
+	var aStr = "";
+	
+	switch( inErrNo)
+	{
 		case 0:
 		case 1:
-			aStr = 'No errors';
+			aStr = "No errors";
 			break;
+	
 		case 2:
-			aStr = 'Low Voltage(LV)';
+			aStr = "Low Voltage(LV)";
 			break;
+			
 		case 4:
-			aStr = 'Over Voltage(OV)';
+			aStr = "Over Voltage(OV)";
 			break;
+			
 		case 8:
-			aStr = 'Motor Driver Protection';
+			aStr = "Motor Driver Protection";
 			break;
 	}
+	
+	
 	return aStr;
-};
+}
 
-Device.prototype.HWnameString = function (inHWNo) {
-	var aStr = '';
-
-	switch (inHWNo) {
+function DEVICE_HWnameString(inHWNo)
+{
+	var aStr = "";
+	
+	switch( inHWNo)
+	{
 		case 0:
-			aStr = 'DSshield with FlashAir';
+			aStr = "DSshield with FlashAir";
 			break;
 		case 1:
-			aStr = 'DSair1';
+			aStr = "DSair1";
 			break;
 		case 2:
-			aStr = 'DSair2';
+			aStr = "DSair2";
 			break;
+			
 		default:
-			aStr = 'Unknown';
+			aStr = "Unknown";
 			break;
 	}
+	
+	
 	return aStr;
-};
+}
--- js/storage.js
+++ js/storage.js
@@ -1,309 +1,458 @@
-//
-var Storage = function () {
-	this._gEnableLoad = 0;
-	this._gLoading = 0;
-	this._dblLocArray = [];
-	this._LocProtocol = this._defaultLocProtocol;
-	this._AccProtocol = this._defaultAccProtocol;
-	this._LocMeterMaxSpeed = 0;
+var gEnableLoad = 0;
+var gLoading = 0;
 
-	this._AccTypes = [];
-	this._MapAccAddr = [];
-	this._MapImage = [];
-	this._MapWidth = 0;
-	this._MapHeight = 0;
-
-	this._onLoadCallbacks = [];
-	window.addEventListener('load', this);
-};
-
-Storage.prototype._defaultLocProtocol = DsairConst.protocolDCC;	// MM2 or DCC
-Storage.prototype._defaultAccProtocol = DsairConst.protocolDCC;	// MM2 or DCC
-// Storageに保存する値
-Storage.prototype._protocolList = [];
-Storage.prototype._protocolList[DsairConst.protocolMM2] = 'MM2';
-Storage.prototype._protocolList[DsairConst.protocolDCC] = 'DCC';
-
-Storage.prototype._maxMapImages = 100 * 50;
-
-Storage.prototype.handleEvent = function (e) {
-	switch (e.type) {
-		case 'load':
-			this.Init();
-			break;
-		default:
-			break;
-	}
-};
-
-// データ読み込み完了コールバック登録
-Storage.prototype.addStorageLoadCallback = function (obj) {
-	this._onLoadCallbacks.push(obj);
-};
-
-//
-
-Storage.prototype.Clear = function () {
-	localStorage.clear();
-};
+function STORE_Clear()
+{
+    localStorage.clear();
+}
 
 
-Storage.prototype.Save = function () {
-	if (this._gEnableLoad == 0) {
-		localStorage.setItem('UNIQUEID', 'DSAIR0001');
-	}
-};
+function STORE_Save()
+{
+    if(gEnableLoad == 0)
+    {
+        localStorage.setItem("UNIQUEID", "DSAIR0001");
+    }
+}
 
-Storage.prototype.SaveLocAddr = function (dblLocArray) {
-	if (this._gLoading == 1) {
-		return;
-	}
-	this.Save();
-	this._dblLocArray = dblLocArray;
-	localStorage.setItem('LOCADDR', this._dblLocArray.join(','));
-};
+function STORE_Save_ProtcolLoc()
+{
+    STORE_Save();
 
-Storage.prototype.SaveProtcolLoc = function (inLocProtocol) {
-	if (this._gLoading == 1) {
-		return;
-	}
-	if (inLocProtocol in this._protocolList) {
-		this.Save();
-		this._LocProtocol = inLocProtocol;
-		localStorage.setItem('PROTCOL_LOC', this._protocolList[this._LocProtocol]);
-	} else {
-		console.info('Invalid loco protocol "%s"', inLocProtocol);
-	}
-};
+    localStorage.setItem("PROTCOL_LOC", LocProtocol);
+}
 
-Storage.prototype.SaveProtcolAcc = function (inAccProtocol) {
-	if (this._gLoading == 1) {
-		return;
-	}
-	
-	if (inAccProtocol in this._protocolList) {
-		this.Save();
-		this._AccProtocol = inAccProtocol;
-		localStorage.setItem('PROTCOL_ACC', this._protocolList[this._AccProtocol]);
-	} else {
-		console.info('Invalid accessory protocol "%s"', inAccProtocol);
-	}
-};
+function STORE_Save_ProtcolAcc()
+{
+   
+    STORE_Save();
 
-Storage.prototype.SaveMaxSpeed = function (LocMeterMaxSpeed) {
-	if (this._gLoading == 1) {
-		return;
-	}
-	this.Save();
-	this._LocMeterMaxSpeed = LocMeterMaxSpeed;
-	localStorage.setItem('MAXSPEED_LOC', this._LocMeterMaxSpeed);
-};
+    localStorage.setItem("PROTCOL_ACC", AccProtocol);
+}
 
-Storage.prototype.SaveTypeImagesAcc = function (AccTypes) {
-	this.Save();
-	this._AccTypes = AccTypes;
-	localStorage.setItem('ACCTYPEIMAGES', this._AccTypes.join(','));
-};
+function STORE_Save_TypeImagesAcc()
+{
+    STORE_Save();
 
-Storage.prototype.SaveMapDatas = function (MapAccAddr, MapImage, MapWidth, MapHeight) {
-	this.Save();
-	this._MapAccAddr = MapAccAddr;
-	this._MapImage = MapImage;
-	this._MapWidth = MapWidth;
-	this._MapHeight = MapHeight;
-	localStorage.setItem('MAPDAT-ACCADDR', this._MapAccAddr.join(','));
-	localStorage.setItem('MAPDAT-IMAGE', this._MapImage.join(','));
-	localStorage.setItem('MAPDAT-WIDTH', this._MapWidth);
-	localStorage.setItem('MAPDAT-HEIGHT', this._MapHeight);
-};
+    localStorage.setItem("ACCTYPEIMAGES", AccTypes);
+}
 
-//
+function STORE_Save_MapDatas()
+{
+    STORE_Save();
 
-Storage.prototype.LoadLocAddr = function () {
-	//console.log(this._AccProtocol);
-	if (this._gEnableLoad == 0) {
-		console.log('_gEnableLoad = 0');
-		return;
-	}
+    localStorage.setItem("MAPDAT-ACCADDR", Map_AccAddr);
+    localStorage.setItem("MAPDAT-IMAGE", Map_Image);
+    localStorage.setItem("MAPDAT-WIDTH", Map_Width);
+    localStorage.setItem("MAPDAT-HEIGHT", Map_Height);
+}
 
-	var aLocArray = localStorage.getItem('LOCADDR');
+function STORE_Save_ROBOTUserScript()
+{
+    STORE_Save();
 
-	if (aLocArray != null) {
-		this._dblLocArray = [];
-		for (var addr of aLocArray.split(',')) {
-			this._dblLocArray.push(parseInt(addr));
-		}
-	} else {
-		console.info('aLocArray = null');
-	}
-};
+    localStorage.setItem("ROBOT_USERSCRIPT", gROBOT_RecordData);
+}
 
-Storage.prototype.LoadProtcol = function () {
-	if (this._gEnableLoad == 0) {
-		return;
-	}
+function STORE_Load_MapDatas()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
 
-	var aLocProtocol = localStorage.getItem('PROTCOL_LOC');
-	var aAccProtocol = localStorage.getItem('PROTCOL_ACC');
+    var aAccAddrArray_str = localStorage.getItem("MAPDAT-ACCADDR");
+    
+    if( aAccAddrArray_str != null)
+    {
 
-	if (aLocProtocol != null) {
-		this._LocProtocol = aLocProtocol;
-	}
+        var aAccAddrArray_strarray = aAccAddrArray_str.split(",");
 
-	if (aAccProtocol != null) {
-		this._AccProtocol = aAccProtocol;
-	}
-};
+        if( aAccAddrArray_strarray.length == 0)
+        {
+            return;
+        }
 
-Storage.prototype.LoadMaxSpeed = function () {
-	if (this._gEnableLoad == 0) {
-		return;
-	}
+        for( var i = 0; i < (100*50); i++)
+        {
+            Map_AccAddr[i] = parseInt(aAccAddrArray_strarray[i]);
+        }
+    }
+    else
+    {
+        for( var i = 0; i < 50 * 100; i++)
+        {
+            Map_AccAddr[i] = 0;
+        }
+    } 
+    
+    var aMapImgArray_str = localStorage.getItem("MAPDAT-IMAGE");
+    
+    if( aMapImgArray_str != null)
+    {
 
-	var aLocMaxSpeed_str = localStorage.getItem('MAXSPEED_LOC');
+        var aMapImgArray_strarray = aMapImgArray_str.split(",");
 
-	if (aLocMaxSpeed_str != null) {
-		var aLocMaxSpeed = parseInt(aLocMaxSpeed_str);
+        if( aMapImgArray_strarray.length == 0)
+        {
+            return;
+        }
 
-		if (aLocMaxSpeed > 0) {
-			this._LocMeterMaxSpeed = aLocMaxSpeed;
-		}
-	}
-};
+        for( var i = 0; i < (100*50); i++)
+        {
+            Map_Image[i] = parseInt(aMapImgArray_strarray[i]);
+        }
+    }
+    else
+    {
+        for( var i = 0; i < 50 * 100; i++)
+        {
+            Map_Image[i] = 0;
+        }
+    }    
+    
+    var aMapWidth = localStorage.getItem("MAPDAT-WIDTH");
+    var aMapHeight = localStorage.getItem("MAPDAT-HEIGHT");
 
-Storage.prototype.LoadTypeImagesAcc = function () {
-	if (this._gEnableLoad == 0) {
-		return;
-	}
+    if( aMapWidth != null)
+    {
+    	Map_Width = aMapWidth;
+    }
+    
+    if( aMapHeight != null)
+    {
+    	Map_Height = aMapHeight;
+    }
+    
+}
 
-	var aAccImageArray_str = localStorage.getItem('ACCTYPEIMAGES');
+function STORE_Load_TypeImagesAcc()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
 
-	if (aAccImageArray_str != null) {
+    var aAccImageArray_str = localStorage.getItem("ACCTYPEIMAGES");
+    
+    if( aAccImageArray_str != null)
+    {
 
-		var aAccImageArray_strarray = aAccImageArray_str.split(',');
+        var aAccImageArray_strarray = aAccImageArray_str.split(",");
 
-		this._AccTypes = [];
-		for (var accType of aAccImageArray_strarray) {
-			this._AccTypes.push(parseInt(accType));
-		}
-		if (aAccImageArray_strarray.length > DsairConst.maxAccessories) {
-			this._AccTypes = this._AccTypes.slice(0, DsairConst.maxAccessories);
-		} else if (aAccImageArray_strarray.length < DsairConst.maxAccessories) {
-			for (var i = aAccImageArray_strarray.length; i < DsairConst.maxAccessories; i++) {
-				this._AccTypes.push(0);
-			}
-		}
-	}
-};
+        if( aAccImageArray_strarray.length == 0)
+        {
+            return;
+        }
 
-Storage.prototype.LoadMapDatas = function () {
-	if (this._gEnableLoad == 0) {
-		return;
-	}
+        for( var i = 0; i < 2044; i++)
+        {
+            AccTypes[i] = parseInt(aAccImageArray_strarray[i]);
+        }
+    }
+}
 
-	var aAccAddrArray_str = localStorage.getItem('MAPDAT-ACCADDR');
 
-	if (aAccAddrArray_str != null) {
-		var aAccAddrArray_strarray = aAccAddrArray_str.split(',');
+function STORE_Load_MaxSpeed()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
 
-		for (var addr of aAccAddrArray_strarray) {
-			this._MapAccAddr.push(parseInt(addr));
-		}
+    var aLocMaxSpeed_str = localStorage.getItem("MAXSPEED_LOC");
 
-		for (var i = aAccAddrArray_strarray.length; i < this._maxMapImages; i++) {
-			this._MapAccAddr.push(0);
-		}
-	}
+    if( aLocMaxSpeed_str != null)
+    {
+        var aLocMaxSpeed = parseInt(aLocMaxSpeed_str);
 
-	var aMapImgArray_str = localStorage.getItem('MAPDAT-IMAGE');
+        if( aLocMaxSpeed > 0)
+        {
+            LocMeterMaxSpeed = aLocMaxSpeed;
+            
+            $(function() {
+                $("#maxspeed_slider").val(aLocMaxSpeed);
+                $("#maxspeed_slider").selectmenu("refresh");
+            });
+        }
+    }
 
-	if (aMapImgArray_str != null) {
+}
 
-		var aMapImgArray_strarray = aMapImgArray_str.split(',');
+function STORE_Save_MaxSpeed()
+{
+     
+    if( gLoading == 1)
+    {
+        return;
+    }
+  
+    STORE_Save();
 
-		for (var image of aMapImgArray_strarray) {
-			this._MapImage.push(parseInt(image));
-		}
-		
-		for (var i = aMapImgArray_strarray.length; i < this._maxMapImages; i++) {
-			this._MapImage.push(0);
-		}
-	}
+    localStorage.setItem("MAXSPEED_LOC", LocMeterMaxSpeed);
+}
 
-	var aMapWidth = localStorage.getItem('MAPDAT-WIDTH');
-	var aMapHeight = localStorage.getItem('MAPDAT-HEIGHT');
+function STORE_Load_OutputCurentLimit()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
 
-	//console.log(aMapWidth, aMapHeight);
-	if (aMapWidth != null) {
-		this._MapWidth = aMapWidth;
-	}
+    var aCurrentLimit_str = localStorage.getItem("MAXSPEED_LOC");
 
-	if (aMapHeight != null) {
-		this._MapHeight = aMapHeight;
-	}
-};
+    if( aCurrentLimit_str != null)
+    {
+        var aCurrentLimit = parseInt(aCurrentLimit_str);
 
-Storage.prototype.Load = function () {
+        if( aCurrentLimit > 0)
+        {
+            LimitOutputCurrent = aCurrentLimit;
+            
+            $(function() {
+                $("#limitcurrent_slider").val(aCurrentLimit);
+                $("#limitcurrent_slider").selectmenu("refresh");
+            });
+        }
+    }
 
-	if (localStorage.getItem('UNIQUEID') == 'DSAIR0001') {
-		// 成功
-		this._gEnableLoad = 1;
-	}
-	else {
-		console.log('Not found settings on your localStorage.');
-		this._gEnableLoad = 0;
-	}
-};
+}
+
+
+function STORE_Save_OutputCurentLimit()
+{
+     
+    if( gLoading == 1)
+    {
+        return;
+    }
+  
+    STORE_Save();
+
+    localStorage.setItem("MAXCURRENT", LimitOutputCurrent);
+
+
+}
+
+
+
+
+function STORE_Load_MotionSpeed()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
+
+    var aLocMotionSpeed_str = localStorage.getItem("MOTIONSPEED_LOC");
+
+    if( aLocMotionSpeed_str != null)
+    {
+        var aLocMotionSpeed = parseInt(aLocMotionSpeed_str);
+
+        if( aLocMotionSpeed > 0)
+        {
+            $(function() {
+                $("#motionspeed_meter").val(aLocMotionSpeed);
+                $("#motionspeed_meter").selectmenu("refresh");
+                LocMeterMotionSpeed = aLocMotionSpeed;
+            });
+        }
+    }
+
+}
+
+function STORE_Save_MotionSpeed()
+{
+     
+    if( gLoading == 1)
+    {
+        return;
+    }
+  
+    STORE_Save();
+
+    localStorage.setItem("MOTIONSPEED_LOC", LocMeterMotionSpeed);
+}
+
+function STORE_Load_ROBOTUserScript()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
+
+    var aUserScript = localStorage.getItem("ROBOT_USERSCRIPT");
+
+    if( aUserScript != null)
+    {    
+
+        gROBOT_RecordData = aUserScript;
+
+    }
+}
+
+
+function STORE_Load_Protcol()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
+
+    var aLocProtocol = localStorage.getItem("PROTCOL_LOC");
+    var aAccProtocol = localStorage.getItem("PROTCOL_ACC");
+
+    if( aLocProtocol != null)
+    {
+        LocProtocol = aLocProtocol;
+
+        $(function() {
+
+            if(aLocProtocol == 0)
+            {
+                //MM2
+                $("#radio2").prop("checked", true).change();
+            }
+            else
+            {
+                //DCC
+                $("#radio1").prop("checked", true).change();
+            }
+        });
+    }
+
+    if( aAccProtocol != null)
+    {
+        AccProtocol = aAccProtocol;
+
+        $(function() {
+
+            if(aAccProtocol == 12287)
+            {
+                //MM2
+                $("#radio2a").prop("checked", true).change();
+            }
+            else
+            {
+                //DCC
+                $("#radio1a").prop("checked", true).change();
+            }
+        });
+    }
+}
+
+
+function STORE_Save_CabSkin()
+{
+    STORE_Save();
+
+    localStorage.setItem("CABSKIN_ID", gSkinID);
+}
+
+function STORE_Load_CabSkin()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
+
+    var aCabSkinID = localStorage.getItem("CABSKIN_ID");
+
+    if( aCabSkinID != null)
+    {    
+
+        gSkinID = Number(aCabSkinID);
+
+    }
+}
+
+function STORE_Save_LocAddr()
+{
+    STORE_Save();
+
+    localStorage.setItem("LOCADDR", dblLocArray);
+}
+
+function STORE_Load_LocAddr()
+{
+    if( gEnableLoad == 0)
+    {
+        return;
+    }
+
+    var aLocArray = localStorage.getItem("LOCADDR");
+
+    if( aLocArray != null)
+    {
+
+        dblLocArray = aLocArray.split(",");
+
+        if( dblLocArray.length == 0)
+        {
+            return;
+        }
+    
+        $(function() {
+    
+    
+            $(radio_adr1_label).text(dblLocArray[0]);
+    
+            if (dblLocArray.length > 1) {
+                $(radio_adr2_label).text(dblLocArray[1]);
+            } else {
+                $(radio_adr2_label).text("-");
+            }
+    
+            if (dblLocArray.length > 2) {
+                $(radio_adr3_label).text(dblLocArray[2]);
+            } else {
+                $(radio_adr3_label).text("-");
+            }
+    
+            if (dblLocArray.length > 3) {
+                $(radio_adr4_label).text(dblLocArray[3]);
+            } else {
+                $(radio_adr4_label).text("-");
+            }
+    
+            //console.log(dblLocArray);
+        });
+    }
+
+    
+}
+
+function STORE_Load()
+{
+
+    if( localStorage.getItem("UNIQUEID") == "DSAIR0001")
+    {
+        //成功
+        gEnableLoad = 1;
+    }
+    else
+    {
+        console.log("Not found settings on your localStorage.");
+        gEnableLoad = 0;
+    }
+}
 
 //Document Ready
-Storage.prototype.Init = function () {
-	this._gLoading = 1;
-
-	this.Load();
-	this.LoadLocAddr();
-	this.LoadProtcol();
-	this.LoadMaxSpeed();
-	this.LoadTypeImagesAcc();
-	this.LoadMapDatas();
-
-	this._gLoading = 0;
-
-	// コールバック呼び出し
-	for (var cbObj of this._onLoadCallbacks) {
-		cbObj.onDataLoad();
-	}
-};
-
-// CabControlから呼ばれるメソッド
-Storage.prototype.getLocAddr = function () {
-	//console.log(this._dblLocArray);
-	return this._dblLocArray;
-};
-
-Storage.prototype.getMaxSpeed = function () {
-	return this._LocMeterMaxSpeed;
-};
-
-// CfgControllから呼ばれるメソッド
-Storage.prototype.getLocProtocol = function () {
-	return this._LocProtocol;
-};
-
-Storage.prototype.getAccProtocol = function () {
-	//console.log(this._AccProtocol);
-	return this._AccProtocol;
-};
-
-//
-Storage.prototype.getTypeImagesAcc = function () {
-	return this._AccTypes;
-};
-
-Storage.prototype.getMapInfo = function () {
-	return {
-		accAddrs: this._MapAccAddr,
-		images: this._MapImage,
-		width: this._MapWidth,
-		height: this._MapHeight
-	};
-};
+function STORE_Init()
+{
+    gLoading = 1;
+    
+    STORE_Load();
+    STORE_Load_CabSkin();
+    STORE_Load_LocAddr();
+    STORE_Load_Protcol();
+    STORE_Load_MotionSpeed();
+    STORE_Load_MaxSpeed();
+    STORE_Load_TypeImagesAcc();
+    STORE_Load_MapDatas();
+    STORE_Load_ROBOTUserScript();
+    STORE_Load_OutputCurentLimit();
+    
+    gLoading = 0;
+}
